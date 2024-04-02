be23b7dccd3e55e48b7437fd23c863bcff900663
Gabriel <gabrielsantolin52@gmail.com>
2024-04-02 11:26:57 -0300

    Fix checkbox to orange style.
    Signed-off-by: Gabriel <gabrielsantolin52@gmail.com>

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -16,7 +16,7 @@ export function AttendeeList() {
           <thead>
             <tr className="border-b border-white/10">
               <th style={{ width: 64 }} className="py-3 px-4 text-sm font-semibold text-left">
-                <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10 checked:bg-orange-400" />
+                <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10 text-orange-400 focus:ring-0 focus:ring-offset-0" />
               </th>
               <th className="py-3 px-4 text-sm font-semibold text-left">Código</th>
               <th className="py-3 px-4 text-sm font-semibold text-left">Participante</th>
@@ -30,7 +30,7 @@ export function AttendeeList() {
               return (
                 <tr key={i} className="border-b border-white/10 hover:bg-white/5">
                   <td className="py-3 px-4 text-sm text-zinc-300">
-                    <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10 checked:bg-orange-400" />
+                    <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10 text-orange-400 focus:ring-0 focus:ring-offset-0" />
                   </td>
                   <td className="py-3 px-4 text-sm text-zinc-300">12345</td>
                   <td className="py-3 px-4 text-sm text-zinc-300">
